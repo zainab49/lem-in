8cb60d46c15dc1632bd7631e0b9bb5aaff8fc338
HaithamMonia <159217447+HaithamMonia@users.noreply.github.com>
2024-09-13 15:44:50 +0300

    Real work started

--- main.go
+++ main.go
@@ -1,42 +1,16 @@
 package main
-
+import "functions"
 import (
-	"flag"
-	"fmt"
 	"log"
 	"os"
-	"strings"
-
-	"github.com/Dias1c/lem-in/internal/lemin"
-	"github.com/Dias1c/lem-in/internal/web"
 )
 
-func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("ERROR: Program takes argument (fileName or --flags)!")
-		os.Exit(1)
-	}
-	argument := os.Args[1]
-	// Set Flags
-	port := flag.String("http", "", "--http=:port\n 1050 < port < 65000\n")
-	filename := flag.String("file", "", "--file=filename\n")
-	flag.Parse()
+func ReadData(filename string) ()
 
-	// Start Program
-	if strings.HasPrefix(argument, "--") { // if has flags
-		if *port != "" {
-			err := web.RunServer(*port)
-			if err != nil {
-				log.Println(err)
-				os.Exit(1)
-			}
-		} else if *filename != "" {
-			lemin.RunProgramWithFile(*filename, true)
-		} else { // Default = Help
-			flag.Usage()
-			os.Exit(1)
-		}
-	} else { // Default
-		lemin.RunProgramWithFile(argument, false)
+func main() {
+	if len(os.Args) !=2 {
+		log.Fatal("Invalid Arguments")
+		return
 	}
+	graph, err := ReadData(os.Args[1])
 }
